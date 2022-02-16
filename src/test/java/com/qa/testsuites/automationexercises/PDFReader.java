c7ad0e32d41f4a855db40f4296e5d2a80a35cf1f
alisha <alishazia23.05@gmail.com>
2022-02-16 10:58:48 +0530

    code for pdf validations

---
+++ src/test/java/com/qa/testsuites/automationexercises/PDFReader.java
@@ -0,0 +1,44 @@
+package com.qa.testsuites.automationexercises;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PDFReader 
+{
+	@Test
+	public void readPDFTest() throws IOException
+	{
+//		URL url = new URL("file:///C:/Users/91953/Downloads/SamplePdfdoc.pdf");
+		URL url = new URL("file:///C://Users//91953//eclipse-workspace//E2EProject//src//main//java//com//qa//resources//gcp.pdf");
+		
+	//	FileInputStream fis = new FileInputStream("C:\\Users\\91953\\eclipse-workspace\\E2EProject\\src\\main\\java\\Resources\\config.properties");
+		
+		
+		InputStream  is = url.openStream();
+		
+		BufferedInputStream fileParse = new BufferedInputStream(is);
+		
+		PDDocument document = null;
+		
+		document = PDDocument.load(fileParse);
+		
+		String pdfContent= new PDFTextStripper().getText(document);
+		
+		System.out.println("pdfContent: "+pdfContent);
+		
+		Assert.assertTrue(pdfContent.contains("Dan"), "The file contains Dan");
+		
+		
+	}
+	
+
+} 
